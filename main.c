7d4b9fe67bd265f5f7ae7f79fa5baa07a3f9a966
hsa52 <you@example.com>
2016-10-04 23:27:22 -0700

    updatin' that main.c

--- main.c
+++ main.c
@@ -3,24 +3,25 @@
 
 int main(int c, char** argv)
 {
-    if(!isnumber(argv[1])==0||argv[1]==NULL)
+    if(!isnumber(argv[2])==0||argv[2]==NULL)
     {
-        fprintf(stderr,"Sorry, the value you passed in for the height was not of numerical value."
-                "Please enter in a numerical value and try again. :(");
+        fprintf(stderr,"Sorry, the width value that you have passed is not of numerical value. :("
+                "Please enter in a numerical value and try again.");
         exit(1);
     }
-    else if(!isnumber(argv[2])==0||argv[2]==NULL)
+    else if(!isnumber(argv[1])==0||argv[1]==NULL)
     {
-        fprintf(stderr,"Sorry, the width value that you have passed is not of numerical value."
-                "Please enter in a numerical value and try again. :(");
+        fprintf(stderr,"Sorry, the value you passed in for the height was not of numerical value. :("
+                "Please enter in a numerical value and try again.");
         exit(1);
+
     }
     int len = strlen(argv[4]);
     char* str = &argv[4][len-3];
     if(strcmp(str,"ppm")!=0||argv[4]==NULL)
     {
-        fprintf(stderr,"Sorry, the outputfile is not in the form of ppm. "
-                "Please choose an output file with the proper ppm format and try again. :(");
+        fprintf(stderr,"Sorry, the output file is not in the proper form. :( "
+                "Please choose an output file with the proper , which is ppm format and try again.");
     }
     Object objects[129];
     read_scene(argv[3],objects);
